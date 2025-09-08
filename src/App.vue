52984749e293274f96810eb164a230fa59c614bf
RocklightZL <3454448283@qq.com>
2025-09-08 17:30:05 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
-<script setup lang="ts">
-</script>
-
 <template>
     <div class="main">
-        
+        <div class="header"></div>
+        <div class="body">
+            <div class="left-column">
+                <div class="todo-list"></div>
+                <div class="bug-list"></div>
+            </div>
+            <div class="main-editor">
+                <div class="editor-header"></div>
+                <div class="editor-body"></div>
+            </div>
+            <div class="right-column"></div>
+        </div>
+        <div class="status-bar"></div>
     </div>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style scoped>
 </style>
