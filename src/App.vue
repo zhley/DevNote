6ac84dee51230c094432950e76ea08145574c5c8
RocklightZL <3454448283@qq.com>
2025-09-17 15:49:48 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,4 @@
 <template>
-    <!-- <div class="main">
-        <div class="header"></div>
-        <div class="body">
-            <div class="left-column">
-                <div class="todo-list"></div>
-                <div class="bug-list"></div>
-            </div>
-            <div class="main-editor">
-                <div class="editor-header"></div>
-                <div class="editor-body"></div>
-            </div>
-            <div class="right-column">
-                <div class="idea-pool"></div>
-            </div>
-        </div>
-        <div class="status-bar"></div>
-    </div> -->
     <workspace/>
 </template>
 
