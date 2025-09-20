d572b49e9c1a39f43a823a8e6fb855032d02e8c2
RocklightZL <3454448283@qq.com>
2025-09-20 17:19:59 +0800

    sql

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,7 +9,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
