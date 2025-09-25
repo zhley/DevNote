b920fdd4afe227fec4e047b7381120a72607bf9b
zhley <3454448283@qq.com>
2025-09-25 18:38:39 +0800

    same

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::WindowEvent;
+
 // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn show_window(window: tauri::Window) {
+    let _ = window.show();
+    let _ = window.set_focus();
 }
 
 fn main() {
@@ -13,7 +21,16 @@ fn main() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                // 阻止窗口关闭，改为隐藏
+                api.prevent_close();
+                let _ = window.hide();
+                
+                // 应用已隐藏到任务栏
+            }
+        })
+        .invoke_handler(tauri::generate_handler![greet, show_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
